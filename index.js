5bcf184393cbeccf6d6cedcceca0a176f8ef2bb1
Swaibu Rumbiiha <rumbiihas@gmail.com>
2020-03-04 19:06:18 +0200

    chore add node-express

--- index.js
+++ index.js
@@ -1,54 +1,27 @@
-const fetch = require("node-fetch");
-var NodeGeocoder = require("node-geocoder");
-const countries = require("./api/assets/countries.json");
 
-var options = {
-  provider: "google",
-  httpAdapter: "https",
-  apiKey: "AIzaSyCcAP0GrIyhCe_kJfKY1Cb4OYOeP5vmhsQ",
-  formatter: null
-};
+const request = require("jsonrequest");
+const express = require("express");
+const app = express();
 
-var geocoder = NodeGeocoder(options);
-
-export default () => {
-  fetch("https://get.client-ip.com/lookup")
-    .then(response => {
-      return response.json();
-    })
-    .then(myJson => {
-      const { latitude, longitude, clientIP } = myJson;
-      geocoder.reverse({ lat: latitude, lon: longitude }, function(
-        err,
-        payload
-      ) {
-        if (err) return res.status(400).send({ status: 400, message: err });
-        const {
-          formattedAddress,
-          latitude,
-          longitude,
-          country,
-          countryCode,
-          city,
-          administrativeLevels
-        } = payload[0];
-        const countryDetails = countries.find(
-          country => country.alpha2Code === countryCode
-        );
-        return {
-          status: 200,
-          data: {
-            clientIP,
-            address: formattedAddress,
-            address2: administrativeLevels.level2long,
-            country,
-            countryCode,
-            city,
-            latitude,
-            longitude,
-            countryDetails: countryDetails
-          }
-        };
-      });
+app.use(async (req, res, next) => {
+  try {
+    const data = await request({
+      url: "https://api.ipdata.co?api-key=test",
+      headers: {
+        "User-Agent": "JsonRequest"
+      }
     });
-};
+    req.userInfo = data;
+    next();
+  } catch (error) {
+    res.status(400).send({ status: 400, message: "something went wrong" });
+  }
+
+  next();
+});
+
+app.get("/", (req, res) =>
+  res.status(200).send({ status: 200, data: req.userInfo })
+);
+
+app.listen(process.env.PORT || 1000);
